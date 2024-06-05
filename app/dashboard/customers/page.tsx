b6081dd51ce8e09e31a934432efe57b44e9822a8
Pyae Sone Phyo <pyaesonephyo@mmstudyonline.com>
2024-06-05 16:39:48 +0630

    add customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,33 @@
+import Pagination from '@/app/ui/invoices/pagination';
+
+import Table from '@/app/ui/customers/table';
+
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
-export default function Page() {
-  return <p>Customer Page</p>;
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div className="w-full">
+      <Suspense>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
